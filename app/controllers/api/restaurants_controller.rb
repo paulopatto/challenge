4336576f6a55698f496637c6b83b2c36b474ce6c
Paulo Patto <paulopatto@paulopatto.com>
2018-05-20 20:24:14 -0300

    Create naive version of restaurant filters.
    Was created scopes to filter on Restaurant model:
    
    * by_sulinary -> Integer -> ActiveRecord::Relation
    * with_seats_available -> Date -> Integer -> ActiveRecord::Relation
    
    Through this approach we've trying to dynamic filters, where the same
    endpoint responds to actual requirements.
    
    Examples of requests:
    
    *Requets full restaurant list*
    
    ```sh
    curl \
      -H 'Content-Type: application/json;charset=UTF-8' \
      -XGET "http://localhost:3000/api/restaurants" | python -mjson.tool
    ```
    
    *Request restaurant list filtered by culinary*
    
    ```sh
    curl \
      -H 'Content-Type: application/json;charset=UTF-8' \
      -XGET "http://localhost:3000/api/restaurants?culinaryId=10" | python -mjson.tool
    ```
    
    *Request to find restaurant list with seats available by seats number
    and date*
    
    ```sh
    curl \
      -H 'Content-Type: application/json;charset=UTF-8' \
      -XGET "http://localhost:3000/api/restaurants?seats=2&date=2018-08-30" | python -mjson.tool
    ```
    
    *Case you want save a date to special ocasion with new affair of
    Tinder with Thailandese food...*
    
    > Considering Thailandese food is id = 6.
    
    ```sh
    curl \
      -H 'Content-Type: application/json;charset=UTF-8' \
      -XGET "http://localhost:3000/api/restaurants?seats=2&date=2018-06-12&culinaryId=6" | python -mjson.tool
    ```

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,18 +1,25 @@
 class Api::RestaurantsController < ApplicationController
   def index
-    @results = params[:culinaryId].present? ? filtered_list : Restaurant.all
-    render json: @results
+    restaurants_scope = Restaurant.all
+
+    if params[:culinaryId].present?
+      restaurants_scope = culinary_filter(restaurants_scope, params[:culinaryId].to_i)
+    end
+
+    if params[:date].present? && params[:seats].present?
+      restaurants_scope = seats_available_filter(restaurants_scope, params[:date].to_date, params[:seats].to_i)
+    end
+
+    render json: restaurants_scope, except: %i[id created_at updated_at]
   end
 
   private
 
-  def filtered_list
-    Restaurant.joins(:culinaries).where({culinaries: { id: params[:culinaryId] }})
+  def culinary_filter(scope, culinary_id = params[:culinaryId])
+    scope.by_culinary(culinary_id.to_i)
   end
 
-  def filter(restaurants)
-    restaurants.select do |restaurant|
-      restaurant.culinary_ids.include?(params[:culinaryId].to_i)
-    end
+  def seats_available_filter(scope, date, seats)
+    scope.with_seats_available(date.to_date, seats.to_i)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,14 @@
 class Restaurant < ApplicationRecord
   has_and_belongs_to_many :culinaries, dependent: :destroy
   has_many :reservations
+
+  scope :by_culinary, ->(culinary_id) { joins(:culinaries).where(culinaries: { id: culinary_id }) }
+
+  scope :with_seats_available, -> (date, seats) do
+    left_outer_joins(:reservations)
+    .select('restaurants.*, COALESCE(SUM(reservations.seats), 0) AS seats_booked')
+    .where('reservations.event_time between :sdate and :edate OR seats_capacity > 0', { sdate: date.beginning_of_day, edate: date.end_of_day })
+    .having('seats_booked <= (restaurants.seats_capacity - :seats_need)', { seats_need: seats })
+    .group('restaurants.id')
+  end
 end
