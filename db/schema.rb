7e56fe79066b2ae953be22d6d7e908bcddd6f532
Paulo Patto <paulopatto@paulopatto.com>
2018-05-20 20:18:08 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412222043) do
+ActiveRecord::Schema.define(version: 20180518004737) do
 
   create_table "culinaries", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180412222043) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seats_capacity"
   end
 
 end
